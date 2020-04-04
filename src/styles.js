8da992b451e28700fb1fe2a33c452c962961c74d
Jonatas Arão de Carvalho Neves <jonatas.arao.de.carvalho.neves@hotmail.com>
2020-04-04 16:41:34 -0300

    Criando Estilos Globais

---
+++ src/styles.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export const GlobalStyle = createGlobalStyle`
+  body{
+    background-color: #eceff1;
+    display: flex;
+    flex-direction: column;
+    min-height: 100%;
+  }
+  html {
+    height: 100%;
+    min-height: 100%;
+  }
+
+  footer {
+      margin-top: auto;
+  }
+`
