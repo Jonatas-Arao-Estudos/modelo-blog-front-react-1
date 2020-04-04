07b6099149741030358b17bcefecf6482aa5fc43
Jonatas Arão de Carvalho Neves <jonatas.arao.de.carvalho.neves@hotmail.com>
2020-04-04 16:39:56 -0300

    Criando Componente PopularSection

---
+++ src/components/PopularSection/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { HorizontalCard } from '../Cards'
+import { PopularSectionTitle } from './styles';
+
+export function PopularSection({ posts }) {
+  return (
+    <>
+        <PopularSectionTitle to="s">Populares</PopularSectionTitle>
+        {posts.map(post => (
+            <HorizontalCard to={post.url} imgSrc={post.imgSrc} page={post.page} title={post.title} description={post.description} />
+        ))}
+    </>
+  );
+}
