46494206a0fd3f2a24a029d794526b4499f881be
Jonatas Arão de Carvalho Neves <jonatas.arao.de.carvalho.neves@hotmail.com>
2020-04-05 03:09:09 -0300

    Melhorias - Pagina Inicio/Index

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,71 +1,66 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { Container, Row, Col } from 'react-bootstrap';
 
-import Header from '../../components/Header';
 import { ImageCard } from '../../components/Cards';
-import { PopularSection } from '../../components/Sections';
-import Footer from '../../components/Footer';
+import { PopularPostsSection } from '../../components/Sections';
+import { LatestPostsAside } from '../../components/Aside';
+import { FooterDescriptionElement } from '../../components/Footer';
 
-import logo from '../../assets/img/Logo.png';
 import Default from '../../assets/img/default.jpg';
+import logo from '../../assets/img/Logo.png';
 
 export default function Inicio() {
 
-  function IndexCards(){ 
-    const countIndexCards = [1, 2, 3, 4, 5, 6];
-    return countIndexCards.map((id) =>
-    (<Col key={id} lg={4} md={6} xs={12} className="mt-3">
-      <ImageCard to="#" imgSrc={Default} page="Página" title="Título da Matéria"/>
-    </Col>)
-  )};
-
-  let headerPages = []
-
-  for(let i = 1; i <= 4; i++){
-    headerPages.push({
-      id: i,
-      name: `Página ${i}`,
-      url: '/'
-    });
-  }
-
-  let posts = []
-
-  for(let i = 0; i <=4; i++){
-    posts.push({
-      id: i,
-      page: 'Página',
-      title: 'Título da Matéria',
-      description: 'Descrição da Matéria',
-      url: '',
-      imgSrc: Default
-    });
-  }
-
-  let footerPages = [
-    {
-      id: 1,
-      name: `Últimas novidades`,
-      url: '/'
-    },
-    {
-      id: 2,
-      name: `Populares`,
-      url: '/'
-    },
-    {
-      id: 3,
-      name: `Contato`,
-      url: '/'
+  const [IndexCardData, setIndexCardData] = useState([]);
+  useEffect(() => {
+    let temp = [];
+    for(let i = 1; i <= 6; i++){
+      temp.push({
+        id: i,
+        page: 'Página',
+        title: 'Título da Matéria',
+        imgSrc: Default,
+        url: '/'
+      });
     }
-  ]
+    setIndexCardData(temp);
+  },[]);
+
+  const [popularPosts, setPopularPosts] = useState([])
+  useEffect(() => {
+    let temp = []
+    for(let i = 0; i <=4; i++){
+      temp.push({
+        id: i,
+        page: 'Página',
+        title: 'Título da Matéria',
+        description: 'Descrição da Matéria',
+        url: '',
+        imgSrc: Default
+      });
+    }
+    setPopularPosts(temp);
+  },[]);
+
+  const [latestPosts, setLatestPosts] = useState([])
+  useEffect(() => {
+    let temp = []
+    for(let i = 0; i <=6; i++){
+      temp.push({
+        id: i,
+        page: 'Página',
+        title: 'Título da Matéria',
+        description: 'Descrição da Matéria',
+        url: '',
+        imgSrc: Default
+      });
+    }
+    setLatestPosts(temp);
+  },[]);
 
   return (
     <>
-    <Header logo={logo} blogName="Design Teste" pages={headerPages} />
-    
-
     <Container className="mt-3" as="section">
       <Row>
         <IndexCards />
@@ -75,12 +70,22 @@ export default function Inicio() {
     <Container className="mt-5">
       <Row>
         <Col lg={8} xs={12} as="section">
-          <PopularSection posts={posts}/>
+          <PopularPostsSection posts={popularPosts}/>
+        </Col>
+        <Col lg={4} xs={12} as="aside">
+          <LatestPostsAside posts={latestPosts}/>
         </Col>
       </Row>
     </Container>
 
-    <Footer logo={logo} blogName="Design Teste" blogDescription="Descrição do Blog" pages={footerPages}/>
+    <FooterDescriptionElement logo={logo} blogName="Design Teste" blogDescription="Descrição do Blog" />
     </>
   );
+
+  function IndexCards(){
+    return IndexCardData.map(({id, page, title, imgSrc, url}) =>
+    (<Col key={id} lg={4} md={6} xs={12} className="mt-3">
+      <ImageCard to={url} imgSrc={imgSrc} page={page} title={title}/>
+    </Col>)
+  )};
 }
