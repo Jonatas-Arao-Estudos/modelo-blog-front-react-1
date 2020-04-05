0a2ce663b55d5cf06de9055a7d9db6e78f40a889
Jonatas Arão de Carvalho Neves <jonatas.arao.de.carvalho.neves@hotmail.com>
2020-04-05 14:43:10 -0300

    Updates

---
+++ precache-manifest.2731bb566ed2c4a3946df662202a912e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e71691cd381b2ecee2a6a4e57ee77502",
+    "url": "/modelo-blog-front-react-1/index.html"
+  },
+  {
+    "revision": "f6d0e454349ccc78a292",
+    "url": "/modelo-blog-front-react-1/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "f6d0e454349ccc78a292",
+    "url": "/modelo-blog-front-react-1/static/js/2.cc87d369.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/modelo-blog-front-react-1/static/js/2.cc87d369.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41235733eea5c9e715e8",
+    "url": "/modelo-blog-front-react-1/static/js/main.2cd5d54d.chunk.js"
+  },
+  {
+    "revision": "1efc65fed3beb4149e7b",
+    "url": "/modelo-blog-front-react-1/static/js/runtime-main.1b57bea9.js"
+  },
+  {
+    "revision": "26002ee65541539341b832fa63c78068",
+    "url": "/modelo-blog-front-react-1/static/media/default.26002ee6.jpg"
+  }
+]);
