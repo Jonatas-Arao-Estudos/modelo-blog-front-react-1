ace61687c873c0854744ea69cb99b2b53694f145
Jonatas Arão de Carvalho Neves <jonatas.arao.de.carvalho.neves@hotmail.com>
2020-04-09 02:35:54 -0300

    Updates

--- precache-manifest.587866582ea7aca6682705e44a4dc8d6.js
+++ precache-manifest.587866582ea7aca6682705e44a4dc8d6.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0057ddef63c5d5e600e265e5b69cc884",
+    "revision": "ab8253fac7406ccf0d7f0c4e97005b51",
     "url": "/modelo-blog-front-react-1/index.html"
   },
   {
-    "revision": "1b2d587ce07b732f8734",
+    "revision": "9f0457db52d78267fe94",
     "url": "/modelo-blog-front-react-1/static/css/2.de424728.chunk.css"
   },
   {
-    "revision": "1b2d587ce07b732f8734",
-    "url": "/modelo-blog-front-react-1/static/js/2.08d1bd56.chunk.js"
+    "revision": "9f0457db52d78267fe94",
+    "url": "/modelo-blog-front-react-1/static/js/2.1c6720ef.chunk.js"
   },
   {
     "revision": "56729d980da09961afe8d497a21eec12",
-    "url": "/modelo-blog-front-react-1/static/js/2.08d1bd56.chunk.js.LICENSE.txt"
+    "url": "/modelo-blog-front-react-1/static/js/2.1c6720ef.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8769402a4d6259cb27e8",
-    "url": "/modelo-blog-front-react-1/static/js/main.5a850459.chunk.js"
+    "revision": "79dd6a9677c9e994072a",
+    "url": "/modelo-blog-front-react-1/static/js/main.79891bb7.chunk.js"
   },
   {
     "revision": "1efc65fed3beb4149e7b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/modelo-blog-front-react-1/precache-manifest.32844d9ee7b8e90e29c437893b47a807.js"
+  "/modelo-blog-front-react-1/precache-manifest.587866582ea7aca6682705e44a4dc8d6.js"
 );
 
 self.addEventListener('message', (event) => {
